edae4d835e0a2ee59f81a853e7da02888457f895
taku-y <taku.yoshioka.4096@gmail.com>
2023-05-03 15:15:35 +0900

    Update doc for usage of eval_with_recorder in border-py-gym-env (laboroai/border#66)

--- border-py-gym-env/src/lib.rs
+++ border-py-gym-env/src/lib.rs
@@ -1,5 +1,259 @@
 //! A wrapper of [gym](https://gym.openai.com) environments on Python.
 //!
+//! This crate uses python>=3.8 and [`gym`](https://pypi.org/project/gym/)>=0.26 via [`PyO3`].
+//! 
+//! ## Example
+//! 
+//! Here is an example of using the crate, where a random controller ([`Policy`]) is applied
+//! to [`cartpole-v1`] environment of [Gym].
+//! 
+//! ```no_run
+//! use anyhow::Result;
+//! use border_core::{
+//!     record::{BufferedRecorder, Record},
+//!     util, Env as _, Policy,
+//! };
+//! use border_py_gym_env::{
+//!     PyGymEnv, PyGymEnvActFilter, PyGymEnvConfig, PyGymEnvDiscreteAct, PyGymEnvDiscreteActRawFilter,
+//!     PyGymEnvObs, PyGymEnvObsFilter, PyGymEnvObsRawFilter,
+//! };
+//! use serde::Serialize;
+//! use std::{convert::TryFrom, fs::File};
+//!
+//! type PyObsDtype = f32;
+//!
+//! type Obs = PyGymEnvObs<PyObsDtype, f32>;
+//! type Act = PyGymEnvDiscreteAct;
+//! type ObsFilter = PyGymEnvObsRawFilter<PyObsDtype, f32, Obs>;
+//! type ActFilter = PyGymEnvDiscreteActRawFilter<Act>;
+//! type Env = PyGymEnv<Obs, Act, ObsFilter, ActFilter>;
+//!
+//! #[derive(Clone)]
+//! struct RandomPolicyConfig;
+//!
+//! struct RandomPolicy;
+//!
+//! impl Policy<Env> for RandomPolicy {
+//!     type Config = RandomPolicyConfig;
+//!
+//!     fn build(_config: Self::Config) -> Self {
+//!         Self
+//!     }
+//!
+//!     fn sample(&mut self, _: &Obs) -> Act {
+//!         let v = fastrand::u32(..=1);
+//!         Act::new(vec![v as i32])
+//!     }
+//! }
+//!
+//! #[derive(Debug, Serialize)]
+//! struct CartpoleRecord {
+//!     episode: usize,
+//!     step: usize,
+//!     reward: f32,
+//!     obs: Vec<f64>,
+//! }
+//!
+//! impl TryFrom<&Record> for CartpoleRecord {
+//!     type Error = anyhow::Error;
+//!
+//!     fn try_from(record: &Record) -> Result<Self> {
+//!         Ok(Self {
+//!             episode: record.get_scalar("episode")? as _,
+//!             step: record.get_scalar("step")? as _,
+//!             reward: record.get_scalar("reward")?,
+//!             obs: record
+//!                 .get_array1("obs")?
+//!                 .iter()
+//!                 .map(|v| *v as f64)
+//!                 .collect(),
+//!         })
+//!     }
+//! }
+//!
+//! fn main() -> Result<()> {
+//!     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+//!     fastrand::seed(42);
+//!
+//!     let env_config = PyGymEnvConfig::default()
+//!         .name("CartPole-v1".to_string())
+//!         .render_mode(Some("human".to_string()))
+//!         .obs_filter_config(<ObsFilter as PyGymEnvObsFilter<Obs>>::Config::default())
+//!         .act_filter_config(<ActFilter as PyGymEnvActFilter<Act>>::Config::default());
+//!     let mut env = Env::build(&env_config, 0)?;
+//!     let mut recorder = BufferedRecorder::new();
+//!     env.set_render(true);
+//!     let mut policy = RandomPolicy;
+//!
+//!     let _ = util::eval_with_recorder(&mut env, &mut policy, 5, &mut recorder)?;
+//!
+//!     let mut wtr = csv::WriterBuilder::new()
+//!         .has_headers(false)
+//!         .from_writer(File::create(
+//!             "border-py-gym-env/examples/random_cartpole_eval.csv",
+//!         )?);
+//!     for record in recorder.iter() {
+//!         wtr.serialize(CartpoleRecord::try_from(record)?)?;
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
+//! 
+//! ### Relevant types
+//! 
+//! First, we define aliases of relevant data types. `PyObsDtype` is the data type
+//! of arrays of continuous observations emitted from the environment in Python.
+//! `Obs` and `Act` are the aliases of observation and action. The type parameter
+//! `f32` means that `PyObsDtype` is converted to `f32`. So an agent must be able to
+//! handle `f32` array as an observation. The type of `PyGymEnvDiscreteAct` is a set of `i32`.
+//! 
+//! ```ignore
+//! type PyObsDtype = f32;
+//!
+//! type Obs = PyGymEnvObs<PyObsDtype, f32>;
+//! type Act = PyGymEnvDiscreteAct;
+//! ```
+//! 
+//! `ObsFilter` is an alias of [`PyGymEnvObsRawFilter`], which convertes Python objects of
+//! observations from the environment into `Obs` ([`PyGymEnvObs`]) by using 
+//! [`PyGymEnvObsFilter::filt`]. Users can define a filter by implementing [`PyGymEnvObsFilter`]
+//! trait with arbitrary data processing.
+//! 
+//! `ActFilter` is an alias of [`PyGymEnvDiscreteActRawFilter`], implementing
+//! [`PyGymEnvActFilter`] trait. The trait has a method that converts `Act`s
+//! ([`PyGymEnvDiscreteAct`]) into Python objects in order for them to be sent to the
+//! environment.
+//! 
+//! ```ignore
+//! type ObsFilter = PyGymEnvObsRawFilter<PyObsDtype, f32, Obs>;
+//! type ActFilter = PyGymEnvDiscreteActRawFilter<Act>;
+//! ```
+//! 
+//! [`PyGymEnv`] has four type parameters of observation, action, observation filter and
+//! action filter.
+//! 
+//! ```ignore
+//! type Env = PyGymEnv<Obs, Act, ObsFilter, ActFilter>;
+//! ```
+//! 
+//! ### Policy
+//! 
+//! `RandomPolicy` is a controller and implements [`Policy`] trait. Here the policy has no
+//! parameter and is dedined as a struct without any field. Structs implementing [`Policy`]
+//! should be initialized with a configuration, regardless of if there are configurable
+//! parameters. `RandomPolicyConfig` is configuration type without any field.
+//! It should be noted that [`Policy::Config`] must implements [`Clone`] trait.
+//! 
+//! ```ignore
+//! #[derive(Clone)]
+//! struct RandomPolicyConfig;
+//!
+//! struct RandomPolicy;
+//! ```
+//! 
+//! There are two methods in [`Policy`] trait. [`Policy::build`] builds an instance
+//! given a `config`. [`Policy::sample`] emits an action (discrete action in this example).
+//! 
+//! ```ignore
+//! impl Policy<Env> for RandomPolicy {
+//!     type Config = RandomPolicyConfig;
+//!
+//!     fn build(_config: Self::Config) -> Self {
+//!         Self
+//!     }
+//!
+//!     fn sample(&mut self, _: &Obs) -> Act {
+//!         let v = fastrand::u32(..=1);
+//!         Act::new(vec![v as i32])
+//!     }
+//! }
+//! ```
+//! 
+//! ### Recording
+//! 
+//! In order to record sequences of states in episodes, we define `CartpoleRecord`.
+//! This type has four fields: index of episode (`episode`), index of steps of interactions
+//! (`step`), immediate reward (`reward`) and observation (`obs`).
+//! 
+//! ```ignore
+//! #[derive(Debug, Serialize)]
+//! struct CartpoleRecord {
+//!     episode: usize,
+//!     step: usize,
+//!     reward: f32,
+//!     obs: Vec<f64>,
+//! }
+//! ```
+//! 
+//! The sequence of states in an episode will be recorded as a sequence of 
+//! [`border_core::record::Record`]. Those are converted to [`CartpoleRecord`]
+//! with a custom converter as show below: 
+//! 
+//! ```ignore
+//! impl TryFrom<&Record> for CartpoleRecord {
+//!     type Error = anyhow::Error;
+//!
+//!     fn try_from(record: &Record) -> Result<Self> {
+//!         Ok(Self {
+//!             episode: record.get_scalar("episode")? as _,
+//!             step: record.get_scalar("step")? as _,
+//!             reward: record.get_scalar("reward")?,
+//!             obs: record
+//!                 .get_array1("obs")?
+//!                 .iter()
+//!                 .map(|v| *v as f64)
+//!                 .collect(),
+//!         })
+//!     }
+//! }
+//! ```
+//! 
+//! ### Run episodes
+//! 
+//! Having defined required components, let us implement the main function.
+//! [`PyGymEnvConfig`] is configuration of an environment.
+//! We set some configurations of, e.g., observation and action filters,
+//! to [`PyGymEnvConfig`]. We also give a string "CartPole-v1", the name of the 
+//! environment for this example. These names are registered in [`gym`].
+//! 
+//! ```ignore
+//! let env_config = PyGymEnvConfig::default()
+//!     .name("CartPole-v1".to_string())
+//!     .render_mode(Some("human".to_string()))
+//!     .obs_filter_config(<ObsFilter as PyGymEnvObsFilter<Obs>>::Config::default())
+//!     .act_filter_config(<ActFilter as PyGymEnvActFilter<Act>>::Config::default());
+//! ```
+//! 
+//! Next, we instantiate an environment, recorder and policy.
+//! [`BufferedRecorder`] implements [`Recorder`], which has a method used to record some
+//! internal variables of functions in the Border library.
+//! 
+//! In this example, `RandomPolicy` can be initialized without `RandomPolicyConfig`,
+//! because `RandomPolicy` has no parameter.
+//! 
+//! ```ignore
+//! let mut env = Env::build(&env_config, 0)?;
+//! let mut recorder = BufferedRecorder::new();
+//! env.set_render(true);
+//! let mut policy = RandomPolicy;
+//! ```
+//! 
+//! With these instances, we can run episodes. `5` means the number of episodes.
+//! The sequence of states for 5 episodes will be recorded in `recorder`.
+//! 
+//! ```ignore
+//! let _ = util::eval_with_recorder(&mut env, &mut policy, 5, &mut recorder)?;
+//! ```
+//! 
+//! Lastly, we convert sequence of [`Record`]s and write them into a CSV file.
+//! 
+//! ```ignore
+//! for record in recorder.iter() {
+//!     wtr.serialize(CartpoleRecord::try_from(record)?)?;
+//! }
+//! ```
+//! 
 //! [`PyGymEnv`] is a wrapper of [gym](https://gym.openai.com) based on [PyO3](https://github.com/PyO3/pyo3).
 //! It supports some [classic control](https://gym.openai.com/envs/#classic_control),
 //! [Atari](https://gym.openai.com/envs/#atari), and [PyBullet](https://github.com/benelot/pybullet-gym)
@@ -24,6 +278,17 @@
 //! [examples](https://github.com/taku-y/border/blob/main/border-py-gym-env/examples) directory.
 //! Examples with `border-tch-agents`, which are collections of RL agents implemented with tch-rs,
 //! are in [here](https://github.com/taku-y/border/blob/main/border/examples).
+//! 
+//! [`PyO3`]: https://github.com/PyO3/pyo3
+//! [`cartpole-v1`]: https://www.gymlibrary.dev/environments/classic_control/cart_pole/
+//! [Gym]: https://www.gymlibrary.dev/#
+//! [`Policy`]: border_core::Policy
+//! [`Policy::Config`]: border_core::Policy::Config
+//! [`Policy::build`]: border_core::Policy::build
+//! [`Policy::sample`]: border_core::Policy::sample
+//! [`BufferedRecorder`]: border_core::record::BufferedRecorder
+//! [`Recorder`]: border_core::record::Recorder
+//! [`Record`]: border_core::record::Record
 mod act_c;
 mod act_d;
 mod atari;
